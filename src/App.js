97dbc538b110436e3cc08d44940181637435a960
herrhanch33 <21701062@handong.edu>
2025-01-18 23:38:08 +0900

    Wedding Invitation Updated

--- src/App.js
+++ src/App.js
@@ -32,8 +32,8 @@ function App() {
       <section className="intro">
         <div className="place">
           <p>
-            2025.03.08 토요일 오후 1시<br />
-            <span>남서울교회 2층 기쁨홀</span>
+            2025.05.04 토요일 오후 12시 30분<br />
+            <span>한동대학교 김영길 GRACE 스쿨</span>
           </p>
         </div>
         <h3>결혼합니다</h3>
@@ -50,8 +50,8 @@ function App() {
       </section>
 
       <section className="family">
-        <p>故 <span>조은상 · 정연실</span> 의 장남 <span>조항진</span></p>
-        <p><span>진극권 · 정은희</span> 의 차녀 <span>진승혜</span></p>
+        <p>故 <span>한상륜 · 이정선</span> 의 장남 <span>한창희</span></p>
+        <p><span>권영익 · 김영순 </span> 의 장녀 <span>권지은</span></p>
       </section>
 
       <img src="slide-images/7.jpg" alt="" className="cover-img" />
@@ -63,7 +63,7 @@ function App() {
           </div>
           <h3>
             <span>신랑</span>
-            조항진
+            한창희
           </h3>
           <div className="button-div">
             <button onClick={() => window.location.href = `tel:010-7795-4500`}>
@@ -78,7 +78,7 @@ function App() {
             <span>신랑 혼주</span>
           </h3>
           <p>
-            <span>어머니</span>정연실
+            <span>어머니</span>이정선
           </p>
           <div className="button-div">
             <button onClick={() => window.location.href = `tel:010-5690-2922`}>
@@ -95,7 +95,7 @@ function App() {
           </div>
           <h3>
             <span>신부</span>
-            진승혜
+            권지은
           </h3>
           <div className="button-div">
             <button onClick={() => window.location.href = `tel:010-7795-4500`}>
@@ -110,7 +110,7 @@ function App() {
             <span>신부 혼주</span>
           </h3>
           <p>
-            <span>아버지</span>진극권
+            <span>아버지</span>권영익
           </p>
           <div className="button-div">
             <button onClick={() => window.location.href = `tel:010-4119-4500`}>
@@ -121,7 +121,7 @@ function App() {
             </button>
           </div>
           <p>
-            <span>어머니</span>정은희
+            <span>어머니</span>김영순
           </p>
           <div className="button-div">
             <button onClick={() => window.location.href = `tel:010-4019-4500`}>
@@ -186,33 +186,32 @@ function App() {
         </Map>
         
         <div style={{ backgroundColor: '#eee', padding: '30px 15px 40px'}}>
-          <h3 className="place">남서울교회 기쁨홀</h3>
+          <h3 className="place">한동대학교 김영길 GRACE 스쿨</h3>
           <p>
-            서울 영등포구 신풍로16길 1 <span onClick={() => copyText('서울 영등포구 신풍로16길 1')}>복사</span>
+            경북 포항시 북구 흥해읍 352-4 남송리 <span onClick={() => copyText('서울 영등포구 신풍로16길 1')}>복사</span>
           </p>
 
-          <h3 className="left" style={{ marginTop: '30px'}}>지하철</h3>
-          <p className="left">신풍역 1번 출구, 보라매역 4번출구</p>
+          <h3 className="left" style={{ marginTop: '30px'}}>KTX</h3>
+          <p className="left">포항 KTX역</p>
 
           <h3 className="left" style={{ marginTop: '30px'}}>버스</h3>
-          <p className="left">보라매공원(농심 앞) 하차 : 150, 5523, 5531, 5633, 5536, 5623 <br />
-          수녀원(신풍역) 하차 : 507, 760, 5616, 5620, 5625, 5627, 5713</p>
+          <p className="left">한동대학교 하차 : 302, 마을버스</p>
 
           <h3 className="left" style={{ marginTop: '30px'}}>주차</h3>
-          <p className="left">남서울교회 B1, B2 지하주차장</p>
+          <p className="left">김영길 GRACE 스쿨 및 한동대학교 주차장</p>
 
-          <h3 className="left" style={{ marginTop: '30px'}}>대절버스 안내</h3>
-          <p className="left">칠성교회 앞 > 남서울교회 앞 | 07:30 출발<br />
-          남서울교회 앞 > 칠성교회 앞 | 15:00 출발</p>
+          <h3 className="left" style={{ marginTop: '30px'}}>대절버스 안장</h3>
+          <p className="left">서울역 한동대학교 | 07:30 출발<br />
+          서울역| 한동대학교 15:00 출발</p>
         </div>
         
       </section>
       
       <section className="after-party">
         <h3>피로연 안내</h3>
-        <p className="date">일시 | 2025.02.08 15:00 ~ 19:00</p>
-        <p className="place">장소 | 천일식당</p>
-        <p className="address">경남 함안군 칠서면 삼칠로 1387 <span onClick={() => copyText('경남 함안군 칠서면 삼칠로 1387')}>복사</span></p>
+        <p className="date">일시 | 2025.03.03 15:00 ~ 19:00</p>
+        <p className="place">장소 | 무</p>
+        <p className="address">경북 포항시 한동대학교 <span onClick={() => copyText('경북 포항시 한동대학')}>복사</span></p>
         <Map
           center={{ lat: 35.3215, lng: 128.4067 }}
           style={{ width: '100%', height: '400px', marginTop: '20px' }}
@@ -245,17 +244,25 @@ function App() {
             <>
             <div>  
               <div>  
-                <h3>신랑 조항진</h3>
-                <p>하나은행 40691082559007 조항진</p>
+                <h3>신랑 한창희</h3>
+                <p>IBK기업은행 15812469401016 한창희</p>
               </div>
-              <button onClick={() => copyText("하나은행 40691082559007")}>복사</button>
+              <button onClick={() => copyText("IBK기업은행 15812469401016")}>복사</button>
             </div>
-            <div style={{ borderTop: 'none'}}>  
+            <div>  
+            <div>  
+              <h3>신랑 부) 한상륜</h3>
+              <p>IBK기업은행 15812469401016 한창희</p>
+            </div>
+            <button onClick={() => copyText("IBK기업은행 15812469401016 한창희")}>복사</button>
+          </div>
+          <div>  
+            {/* <div style={{ borderTop: 'none'}}>   */}
               <div>  
-                <h3>신랑 모) 정연실</h3>
-                <p>하나은행 40691082559007 조항진</p>
+                <h3>신랑 모) 이정선</h3>
+                <p>IBK기업은행 15812469401016 한창희</p>
               </div>
-              <button onClick={() => copyText("하나은행 40691082559007")}>복사</button>
+              <button onClick={() => copyText("IBK기업은행 15812469401016 한창희")}>복사</button>
             </div>
             </>
           )}
@@ -273,24 +280,24 @@ function App() {
           </h2> 
           <div>  
             <div>  
-              <h3>신부 진승혜</h3>
-              <p>카카오뱅크 3333122546350 진승혜</p>
+              <h3>신부 권지은</h3>
+              <p>국민은행 59420204088297 권지은</p>
             </div>
-            <button onClick={() => copyText("카카오뱅크 3333122546350")}>복사</button>
+            <button onClick={() => copyText("국민은행 59420204088297 권지은")}>복사</button>
           </div>
           <div>  
             <div>  
-              <h3>신부 부) 진극권</h3>
-              <p>농협 80506052014445 진극권</p>
+              <h3>신부 부) 권영익</h3>
+              <p>국민은행 59420204088297 권지은</p>
             </div>
-            <button onClick={() => copyText("농협 80506052014445")}>복사</button>
+            <button onClick={() => copyText("국민은행 59420204088297 권지은")}>복사</button>
           </div>
           <div>  
             <div>  
-              <h3>신부 모) 정은희</h3>
-              <p>농협 74105552014186 정은희</p>
+              <h3>신부 모) 김영순</h3>
+              <p>국민은행 59420204088297 권지은</p>
             </div>
-            <button onClick={() => copyText("농협 74105552014186")}>복사</button>
+            <button onClick={() => copyText("국민은행 59420204088297 권지은")}>복사</button>
           </div>
         </div>
       </section>
@@ -298,9 +305,9 @@ function App() {
       <section className="guestbook">
         <p>
           Edited by<br />
-          Hangjin & Seunghye
+          Changhee & Jieun
         </p>
-        <img src="slide-images/22.jpg" alt="" />
+        <img src="slide-images/13.jpg" alt="" />
         {/* <Guestbook /> */}
       </section>
     </div>
